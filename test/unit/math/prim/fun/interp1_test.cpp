dc9fd2f56dbcc1b30876c3ba3f9a79fe0a267b6b
Hyunji Moon <30194633+hyunjimoon@users.noreply.github.com>
2020-11-11 20:13:22 +0900

    Update interp1_test.cpp

--- test/unit/math/prim/fun/interp1_test.cpp
+++ test/unit/math/prim/fun/interp1_test.cpp
@@ -3,11 +3,6 @@
 #include <test/unit/math/expect_near_rel.hpp>
 #include <vector>
 
-// This file goes in test/unit/math/prim/fun/
-//
-// To run the test type:
-//  ./runTest.py test/unit/math/prim/fun/interp1_test.cpp
-
 TEST(MathPrim, interp1_test) {
 using stan::test::expect_near_rel;
 std::vector<double> x = {0.00, 1.25, 2.50, 3.75, 5.00};
@@ -19,6 +14,4 @@ std::vector<double> y_ref = {0.7591876955, 0.8228001283, 0.7106361362, -0.649033
 std::vector<double> y_test = stan::math::interp1(x, y, x_test);
 
 stan::test::expect_near_rel("test Interp1", y_test, y_ref);
-for (auto i: y_test)
-    std::cout << i << ' ';
 }
