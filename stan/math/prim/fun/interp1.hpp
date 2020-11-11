6f051bab165af54838f1adfa5ca6887218ade37a
Hyunji Moon <30194633+hyunjimoon@users.noreply.github.com>
2020-11-11 20:09:06 +0900

    change every input type to template

--- stan/math/prim/fun/interp1.hpp
+++ stan/math/prim/fun/interp1.hpp
@@ -1,13 +1,11 @@
 #ifndef STAN_MATH_PRIM_FUN_INTERP1_HPP
 #define STAN_MATH_PRIM_FUN_INTERP1_HPP
 
-//#include <stan/math/prim/fun/sort_asc.hpp>
-
 using namespace std;
 namespace stan {
     namespace math {
-        template <typename T>
-        inline std::vector<T> interp1(const std::vector<double>& xData, const std::vector<double>& yData, const std::vector<T>& xTest, std::ostream* pstream__)
+        template <typename T1, typename T2, typename T3>
+        inline std::vector<return_type_t<T1, T2, T3>> interp1(const std::vector<T1>& xData, const std::vector<T2>& yData, const std::vector<T3>& xTest, std::ostream* pstream__ = nullptr)
         {
             using stan::math::index_type_t;
             check_size_match("interp1", "x", xData.size(), "y", yData.size());
@@ -15,13 +13,13 @@ namespace stan {
             int N = xData.size();
             std::vector<double> dydx(N);
             int M = xTest.size();
-            std::vector<T> yVals(M);
+            std::vector<return_type_t<T1, T2, T3>> yVals(M);
 
             for (int i = 0; i < N - 1; i++){
                 dydx[i] = (yData[i+1] -  yData[i]) / (xData[i+1] - xData[i]);
             }
             for(int j = 0; j < M; j ++){
-                T x = xTest[j];
+                T3 x = xTest[j];
                 int i= 0;
                 while(x > xData[i]){
                     i++;
